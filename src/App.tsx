5bbec6f81e2910dc01c4b1e5fb3205b384a47af4
Aldi <dinataaldi69@gmail.com>
2025-09-09 07:20:01 +0700

    feat(auth): buat kerangka login dengan useForm, zod, dan zodResolver
    1. Membuat struktur dasar form login
    2. Integrasi validasi zod dengan zodResolver
    3. Menambahkan aturan validasi email & password (sementara)
    4. Menyiapkan fungsi awal untuk proses login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import LoginPage from "./pages/LoginPage"
 
 const App = () => {
   return (
     <div>
+      <LoginPage />
     </div>
   )
 }
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,81 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import z from "zod"
+
+const LoginPage = () => {
+    const [showPassword, setShowPassword] = useState<boolean>()
+
+    const users = [
+        { email: "aldi@gmail.com", password: "alditayo" },
+        { email: "budi@gmail.com", password: "budi123" },
+        { email: "siti@gmail.com", password: "siti456" },
+    ]
+
+    //validasi
+    const loginFormSchema = z.object({
+        email: z.string().nonempty("Email Tidak Boleh Kosong").min(5, "Email terlalu pendek").max(50, "Email terlalu panjang"), //agar field tidak kosong
+        password: z.string().nonempty("Password Tidak Boleh Kosong")
+    }).superRefine((arg, ctx) => { //cek email dan password
+        const foundUser = users.find(user => user.email === arg.email)
+        console.log(foundUser?.email, foundUser?.password)
+        if (!foundUser?.email) {
+            ctx.addIssue({ //memberi tau zod kalau ada yang error
+                code: "custom",
+                path: ["email"],
+                message: "Email Anda Salah"
+            })
+        } else if (foundUser?.password !== arg.password) {
+            ctx.addIssue({
+                code: "custom",
+                path: ["password"],
+                message: "Password Anda Salah"
+            })
+        }
+    })
+
+    type LoginFormSchema = z.infer<typeof loginFormSchema>
+
+    const form = useForm({
+        resolver: zodResolver(loginFormSchema)
+    })
+
+    const handleLoginUser = (values: LoginFormSchema) => {
+        alert("Form submitted")
+        console.log(values)
+    }
+
+    return (
+        <div className="mt-40">
+            <h1>SISTEM MANAJAMEN CLUB TAEKWONDO</h1>
+            <img src="#" alt="logoTaekwondo" />
+            <form onSubmit={form.handleSubmit(handleLoginUser)} className="flex flex-col">
+                <label htmlFor="#" className="flex flex-col">
+                    Email
+                    <input type="email" {...form.register("email")} className="border w-1/2 text-sm py-1 px-2" placeholder="Masukan Email" />
+                    <span className="text-red-500 text-xs">{form.formState.errors.email?.message}</span>
+                </label>
+                <label htmlFor="#" className="flex flex-col">
+                    Password
+                    <div>
+                        <input type={showPassword ? "text" : "password"} {...form.register("password")} className="border w-1/2 text-sm py-1 px-2 mr-2" placeholder="Masukan Password" />
+                        <input onClick={() => setShowPassword(!showPassword)} type="checkbox" />
+                    </div>
+                    <span className="text-red-500 text-xs">{form.formState.errors.password?.message}</span>
+                </label>
+                <a href="#" className="text-sm text-blue-500">Lupa Password</a>
+                <button className="bg-gray-500 text-white py-1 px-4 w-1/2">Login</button>
+                <label htmlFor="#">
+                    <input type="checkbox" />
+                    Ingatkan saya
+                </label>
+                <label htmlFor="#" className="mt-10">
+                    Masuk sebagai pelatih/manager
+                    <button className="bg-gray-500 text-white py-1 px-4 w-1/2">Masuk Sebagai Pelatih</button>
+                </label>
+            </form>
+        </div>
+    )
+}
+
+export default LoginPage
